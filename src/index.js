c38658d9c1a44c0e9c2410415fdc4c4fc7000997
Jose Luis Pitarch Miravete <jpitarch@mercadona.es>
2025-02-17 19:44:58 +0100

    feat: read csv from bucket

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env) {
+    try {
+      const csvFileName = "el-castell-schedule.csv";
+
+      const object = await env.MY_BUCKET.get(csvFileName);
+      if (!object) {
+        return new Response("Archivo CSV no encontrado", { status: 404 });
+      }
+
+      const csvText = await object.text();
+
+      const jsonData = processFile(csvText);
+
+      return new Response(JSON.stringify(jsonData, null, 2), {
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (error) {
+      return new Response(`Error: ${error.message}`, { status: 500 });
+    }
+  },
 };
+
+const processFile = (file) => {
+  const lines = file.trim().split("\n");
+  const headers = lines[0].split(";").map(header => header.trim());
+
+  return lines.slice(1).map(line => {
+    const values = line.split(";").map(value => value.trim()); // Obtener valores limpios
+    const current = {};
+    
+    headers.forEach((header, index) => {
+      current[header] = values[index] !== undefined ? values[index] : null;
+    });
+
+    return current;
+  });
+}
